6e10e168b03e9e6cd679f143509a9687447a294d
Monali Dhembare <monadhembare19@gmail.com>
2023-11-21 23:31:39 +0530

    first commit: added all local files

---
+++ src/Electricity/Conn.java
@@ -0,0 +1,24 @@
+package Electricity;
+import java.sql.*;
+
+public class Conn {
+	Connection c;
+	Statement s;
+	public Conn() {
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs", "root", "root");
+			s=c.createStatement();
+			
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	public static void main(String[] args) {
+//		Conn cc=new Conn();
+//		System.out.println("hjgn");
+	
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BillingSystem {
+	requires java.sql;  //for sql package
+	requires java.desktop;
+	requires java.xml.crypto;
+	requires rs2xml;  //for java.swing pkg
+}
